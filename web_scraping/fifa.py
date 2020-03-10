09d3ecb0288e33603252c66ce882406a1581a1ed
FIDEL LASCANO <fidekof@gmail.com>
2020-03-10 17:22:45 -0500

    Corrigiendo la consulta

--- web_scraping/fifa.py
+++ web_scraping/fifa.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 if __name__ == '__main__':
-    url = "https://www.easports.com/fifa/ultimate-team/api/fut/item?page="
+    url = "https://www.easports.com/fifa/ultimate-team/api/fut/item?page={}"
     r = requests.get(url + '1')
     items = []
     if r is not None:
@@ -12,12 +12,13 @@ if __name__ == '__main__':
         if cont is not None:
             pages = int(cont['totalPages'])
             for page in range(1, pages + 1):
-                r = requests.get(url + '1')
+                r = requests.get(url.format(page))
                 result = r.json()
                 items += result['items']
-                
-                
-                
+                print('--------------------------\n')
+                print('Busqueda {}'.format(page) + '\nHeaders' + str(r.headers))
+            file_data = {'players': items}
+
             with open('fifa/items/fifa.items.json', 'w') as outfile:
-                json.dump(items, outfile)
+                json.dump(file_data, outfile)
                 outfile.close()
